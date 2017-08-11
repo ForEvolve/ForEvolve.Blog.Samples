cd5efed3d9370596d87b92d85c680f9c61ab1755
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-08-11 00:09:28 -0400

    4. NinjaApi - The ClanService

---
+++ 4. NinjaApi - The ClanService/src/ForEvolve.Blog.Samples.NinjaApi/Services/ClanService.cs
@@ -0,0 +1,40 @@
+﻿using ForEvolve.Blog.Samples.NinjaApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ForEvolve.Blog.Samples.NinjaApi.Services
+{
+    public class ClanService : IClanService
+    {
+        public Task<Clan> CreateAsync(Clan clan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Clan> DeleteAsync(string clanName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsClanExistsAsync(string clanName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Clan>> ReadAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Clan> ReadOneAsync(string clanName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Clan> UpdateAsync(Clan clan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
