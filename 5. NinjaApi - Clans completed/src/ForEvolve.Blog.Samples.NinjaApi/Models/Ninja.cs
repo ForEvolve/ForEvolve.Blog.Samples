63ada1e6cb71caf9a8ddceed54ce3b87d523399d
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-08-11 00:16:48 -0400

    5. NinjaApi - Clans completed

---
+++ 5. NinjaApi - Clans completed/src/ForEvolve.Blog.Samples.NinjaApi/Models/Ninja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForEvolve.Blog.Samples.NinjaApi.Models
+{
+    public class Ninja
+    {
+        public string Key { get; set; }
+        public Clan Clan { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ 5. NinjaApi - Clans completed/test/ForEvolve.Blog.Samples.NinjaApi.IntegrationTests/StartupTest.cs
@@ -0,0 +1,34 @@
+﻿using ForEvolve.Blog.Samples.NinjaApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace ForEvolve.Blog.Samples.NinjaApi.IntegrationTests
+{
+    public class StartupTest : BaseHttpTest
+    {
+        public class ServiceProvider : StartupTest
+        {
+            [Fact]
+            public void Should_return_both_Iga_and_Kōga_clans()
+            {
+                // Arrange
+                var serviceProvider = Server.Host.Services;
+
+                // Act
+                var clans = serviceProvider.GetService<IEnumerable<Clan>>();
+
+                // Assert
+                Assert.NotNull(clans);
+                Assert.Equal(2, clans.Count());
+                Assert.Collection(clans,
+                    clan => Assert.Equal("Iga", clan.Name),
+                    clan => Assert.Equal("Kōga", clan.Name)
+                );
+            }
+        }
+    }
+}
