ed8a95f0a2077a8c41363ad5fa8bcb631c1e7915
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-08-24 14:42:03 -0400

    Updated parts 6-8
    - Renamed 7 to 8
    - Renamed 6
    - Copied 6 to 7

---
+++ 7. NinjaApi - NinjaService/README.md
@@ -0,0 +1,4 @@
+# Dependency Injection
+This project contains the code sample of the 
+[Design Patterns: Asp.Net Core Web API, services, and repositories | Part 6: Implementating the ninjas](http://www.forevolve.com/en/articles/2017/08/30/design-patterns-web-api-service-and-repository-part-6/)
+article.
---
+++ 7. NinjaApi - NinjaService/src/ForEvolve.Blog.Samples.NinjaApi/Services/INinjaService.cs
@@ -0,0 +1,17 @@
+﻿using ForEvolve.Blog.Samples.NinjaApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForEvolve.Blog.Samples.NinjaApi.Services
+{
+    public interface INinjaService
+    {
+        Task<IEnumerable<Ninja>> ReadAllAsync();
+        Task<IEnumerable<Ninja>> ReadAllInClanAsync(string clanName);
+        Task<Ninja> ReadOneAsync(string clanName, string ninjaKey);
+        Task<Ninja> CreateAsync(Ninja ninja);
+        Task<Ninja> UpdateAsync(Ninja ninja);
+        Task<Ninja> DeleteAsync(string clanName, string ninjaKey);
+    }
+}
