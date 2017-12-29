353b84de40b53e79ed17b2e1f4ddf0daaed580cf
Carl-Hugo Marcotte <carlhugo@gmail.com>
2017-12-29 13:48:55 -0500

    More moving around and renaming
    Added "02-Basic with Resources" sample (copy of 01)

---
+++ Localization - Samples/02-Basic with Resources/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore.Localization
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register localization services & options with ASP.NET Core DI container
+            services
+                .AddForEvolveLocalization();
+
+            // MVC
+            services
+                .AddMvc()
+                .AddForEvolveMvcLocalization(); // Configure MVC to use ForEvolve Localization, based on configuration
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            // Call UseRequestLocalization(options):
+            // Adds the Microsoft.AspNetCore.Localization.RequestLocalizationMiddleware to automatically
+            // set culture information for requests based on information provided by the client.
+            app.UseForEvolveRequestLocalization();
+
+            // MVC
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
